f2fea6ae1336a19e45a982097d560327fe02382f
Yangguang <liyangguangcn@gmail.com>
2016-04-11 09:53:47 -0400

    move to github

---
+++ js/script.js
@@ -0,0 +1,32 @@
+(function(){
+	$(document).ready(function() {
+
+		$(".js-crumb, .js-content, .navlist").on("click", "a", function(event) {
+			openPage($(this).attr('data-link'), $(this).html(), $(this).attr('data-nav'));
+		});
+
+		$(".navitem:eq(0)>a").click();
+
+		$.ajaxSetup ({
+		    // Disable caching of AJAX responses
+		    cache: false
+		});
+	});
+
+	function openPage(linkName, title, superNav){
+		var crumb = '<a data-link="program">Data Science</a> > ';
+		crumb += !!superNav ? ('<a data-link="degrees">' + superNav + '</a> > ') : '';
+		crumb += title;
+		$('.js-crumb').html(crumb);
+
+		$(".navlist li").removeClass('active');
+		$(".navlist a[data-link="+linkName+"]").parent("li").addClass('active');
+
+		loadPageContent(linkName);
+	}
+
+	function loadPageContent(linkName){
+		$( ".js-content" ).load( "pages/"+linkName+".html" );
+	}
+	
+})();
