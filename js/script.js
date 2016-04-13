c91c1b8fc315973e4e2125c7596cfa785b90bf71
Yangguang <liyangguangcn@gmail.com>
2016-04-13 10:33:57 -0400

    new images, changed sidebar, some content changes

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 (function(){
-	$(document).ready(function() {
+  $(document).ready(function() {
 
-		// event listener for navigation related links
-		$(".js-crumb, .js-content, .navlist").on("click", "a", function(event) {
-			if ($(this).attr('data-link')) {
-				openPage($(this).attr('data-link'), $(this).html(), $(this).attr('data-nav'));
-			}
-		});
+    // event listener for navigation related links
+    $("body").on("click", "a[data-link]", function(event) {
+      openPage($(this).attr('data-link'), $(this).html(), $(this).attr('data-nav'));
+    });
 
-		// event listener for expand course list
-		$('.js-content').on('click', '.expand-button', function(event) {
-			$(this).next('ul').removeClass('hide');
-			$(this).remove();
-		});
+    // event listener for expand course list
+    $('.js-content').on('click', '.expand-button', function(event) {
+      $(this).next('ul').removeClass('hide');
+      $(this).remove();
+    });
 
-		// load the first page
-		$(".navitem:eq(0)>a").click();
+    // load the first page
+    $(".navitem:eq(0)>a").click();
 
-		// Disable caching of AJAX responses
-		$.ajaxSetup ({ cache: false });
+    // Disable caching of AJAX responses
+    $.ajaxSetup ({ cache: false });
 
-	});
+  });
 
-	function openPage(linkName, title, superNav){
-		var crumb = '<a data-link="program">Data Science</a> > ';
-		crumb += !!superNav ? ('<a data-link="degrees">' + superNav + '</a> > ') : '';
-		crumb += title;
-		$('.js-crumb').html(crumb);
+  function openPage(linkName, title, superNav){
+    var crumb = '<a data-link="program">Data Science</a> > ';
+    crumb += !!superNav ? ('<a data-link="degrees">' + superNav + '</a> > ') : '';
+    crumb += title;
+    $('.js-crumb').html(crumb);
 
-		$(".navlist li").removeClass('active');
-		$(".navlist a[data-link="+linkName+"]").parent("li").addClass('active');
+    $(".navlist li").removeClass('active');
+    $(".navlist a[data-link="+linkName+"]").parent("li").addClass('active');
 
-		loadPageContent(linkName);
-	}
+    loadPageContent(linkName);
+  }
 
-	function loadPageContent(linkName){
-		$( ".js-content" ).load( "pages/"+linkName+".html" );
-	}
-	
+  function loadPageContent(linkName){
+    $( ".js-content" ).load( "pages/"+linkName+".html" );
+  }
+  
 })();
