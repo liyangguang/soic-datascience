1c3dc116c478916bedee452b83f19fcb97e39829
Yangguang <liyangguangcn@gmail.com>
2016-04-19 08:24:13 -0400

    separate out talks

--- js/script.js
+++ js/script.js
@@ -23,39 +23,6 @@
     // Disable caching of AJAX responses
     $.ajaxSetup ({ cache: false });
 
-    // talks page: expand description
-    $('.js-toggle-button').on('click', function(event) {
-      $(this).parents(".talk-description").toggleClass("hideContent").toggleClass("showContent");
-      $(this).toggleClass("toggle-button-more").toggleClass("toggle-button-less");
-    });
-
-    // show video
-    $('.js-button-video').on('click', function(){
-      $('.js-video-wrap').removeClass('hide');
-    });
-
-    // close video
-    $('.js-video-close').on('click', function(){
-      $('.js-video-wrap').addClass('hide');
-      document.getElementById('talk-video').pause();
-    });
-
-    // chromecast video
-    document.addEventListener("DOMContentLoaded", function() {
-      videojs("demo_player", {
-        plugins: {
-          chromecast: {
-            enabled: true,
-            appId: 34523314,
-            metadata: {
-              title: "Jure Leskovec",
-              subtitle: "Subtitle"
-            }
-          }
-        }
-      });
-    });
-
   });
 
   function openPage(linkName, title, superNav){
