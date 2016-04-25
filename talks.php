9aaefe8c51ab4c7108d13ca9c38c2a211d2377d4
Yangguang <liyangguangcn@gmail.com>
2016-04-25 09:54:12 -0400

    recovered talks, changed to php

--- talks.php
+++ talks.php
@@ -1,36 +1,27 @@
-<!doctype html>
-<html>
-	<meta charset="UTF-8">
-	<title>Data Science</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/soic.css">
+<?php
+  $title = 'Data Science Invited Talks';
+  include('header.html');
+?>
+	<link rel="stylesheet" href="css/talks.css">
 	<link rel="stylesheet" href="css/talks.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 	<link href="https://fonts.iu.edu/style.css?family=BentonSans:regular,bold|BentonSansCond:regular|BentonSansComp:bold|SalvoSerifCond:bold" rel="stylesheet" type="text/css">
 
-	<body>
+  <!-- stylesheets -->
+    <link href="//multimedia.datascience.indiana.edu/video-js.css" rel="stylesheet" type="text/css">
+    <link href="//multimedia.datascience.indiana.edu/videojs.chromecast.css" rel="stylesheet" type="text/css">
 
-		<div class="iu-bar">
-			<div class="container">
-				<img class="iu-logo" src="http://assets.iu.edu/brand/legacy/trident-large.png" height="73" width="64" alt=" ">
-				<p class="iu-name">Indiana University</p>
-			</div>
-		</div>
+    <!-- javascripts -->
+    <script src="http://www.gstatic.com/cv/js/sender/v1/cast_sender.js"></script>
 
-		<header>
-			<div class="container">
-				<h1 class="header-h1">Data Science Invited Talk Series</h1>
-				<h2 class="header-h2">School of Informatics and Computing</h2>
-				<img class="header-social" src="image/social.png" alt="">
-			</div>
-		</header>
-
+    <script src="//multimedia.datascience.indiana.edu/video.dev.js"></script>
+    <script src="//multimedia.datascience.indiana.edu/videojs.chromecast.js"></script>
 		<main class="container">
 <!-- 			<div class="row">
 				<img src="http://wptest.soic.indiana.edu/files/2016/04/cropped-ds-temp-image.jpg" alt="" class="full-width">
 			</div> -->
 			<p class="crumb">
-				<a href="http://www.soic.indiana.edu/graduate/degrees/data-science/index.html">Data Science</a> > Invited Talks
+				<a href="index.php">Data Science</a> > Invited Talks
 			</p>
 			<h1 class="content-heading">Upcoming <button class="button-subscribe">Get notification for future talks!</button></h1>
 				<div class="card row">
@@ -65,7 +56,7 @@
 						<a class="talk-location" target="_blank" href="https://www.google.com/maps/place/150+S+Woodlawn+Ave,+Bloomington,+IN+47405/@39.1653408,-86.5257846,17z/data=!3m1!4b1!4m2!3m1!1s0x886c66c1de66055b:0xd0337a05403f2833">Lindley Hall 102</a><br>
 						<button class="talk-button js-button-video">
 							<i class="fa fa-play-circle-o"></i>
-							<span>Video On Demand</span>
+							<span>On Demand</span>
 						</button>
 					</div>
 					<div class="talk-profile col-xs-2">
@@ -79,7 +70,7 @@
 						<h3 class="talk-title">Point, Line & Data: New methods for understanding complex data, from storytelling to machine learning</h3>
 						<p class="talk-description hideContent">
 							The aesthetics of science is changing, the diffusion of data visualization tools is enabling a revival of beauty in scientific research. More and more papers are presented with seductive images, convincing videos, and sharp interactive tools. Scientific storytelling will be discussed with 2 case studies: "Charting Culture, 2014", and "Rise of partisanship, 2015". In the second part of the talk we explore the connection between Machine Learning & Data Visualization. We will see together 3 projects: News Explorer - exploration of real-time news, Ted Watson - exploration of a large corpus of videos, and Watson 500 - the analysis of relationships between entities and topics in a specific corpus of date. We encourage the public to use these tools before the talk: <a href="http://news-explorer.mybluemix.net/">IBM Watson News Explorer</a>, <a href="http://watson.ted.com/">Watson TED</a>, <a href="http://watson500.mybluemix.net/">IBM Watson 500</a><br><br>
-							<b>Speaker:</b> Mauro Martino is an Italian expert in data visualization based in Boston. He created and leads the Cognitive Visualization Lab at IBM Watson in Cambridge, Massachusetts, USA. Martino’s data visualizations have been published in the scientific journals Nature, Science, and the Proceedings of the National Academy of Sciences. His projects have been shown at international festivals including Ars Electronica, and art galleries including the Serpentine Gallery, UK, GAFTA, USA, and the Lincoln Center, USA.<a class="showmore js-showmore">... show more</a>
+							<b>Speaker:</b> Mauro Martino is an Italian expert in data visualization based in Boston. He created and leads the Cognitive Visualization Lab at IBM Watson in Cambridge, Massachusetts, USA. Martino’s data visualizations have been published in the scientific journals Nature, Science, and the Proceedings of the National Academy of Sciences. His projects have been shown at international festivals including Ars Electronica, and art galleries including the Serpentine Gallery, UK, GAFTA, USA, and the Lincoln Center, USA.<a class="toggle-button js-toggle-button toggle-button-more"></a>
 						</p>
 					</div>
 				</div>
@@ -89,7 +80,7 @@
 						<a class="talk-location" target="_blank" href="https://www.google.com/maps/place/Woodburn+Hall,+Bloomington,+IN+47406/@39.1680905,-86.5222573,17z/data=!3m1!4b1!4m2!3m1!1s0x886c66c0837a7dd7:0x147532cb28315aaf">Woodburn Hall, 200</a><br>
 						<button class="talk-button js-button-video">
 							<i class="fa fa-play-circle-o"></i>
-							<span>Video On Demand</span>
+							<span>On Demand</span>
 						</button>
 					</div>
 					<div class="talk-profile col-xs-2">
@@ -103,30 +94,20 @@
 						<h3 class="talk-title">Point, Line & Data: New methods for understanding complex data, from storytelling to machine learning</h3>
 						<p class="talk-description hideContent">
 							The aesthetics of science is changing, the diffusion of data visualization tools is enabling a revival of beauty in scientific research. More and more papers are presented with seductive images, convincing videos, and sharp interactive tools. Scientific storytelling will be discussed with 2 case studies: "Charting Culture, 2014", and "Rise of partisanship, 2015". In the second part of the talk we explore the connection between Machine Learning & Data Visualization. We will see together 3 projects: News Explorer - exploration of real-time news, Ted Watson - exploration of a large corpus of videos, and Watson 500 - the analysis of relationships between entities and topics in a specific corpus of date. We encourage the public to use these tools before the talk: <a href="http://news-explorer.mybluemix.net/">IBM Watson News Explorer</a>, <a href="http://watson.ted.com/">Watson TED</a>, <a href="http://watson500.mybluemix.net/">IBM Watson 500</a><br><br>
-							<b>Speaker:</b> Mauro Martino is an Italian expert in data visualization based in Boston. He created and leads the Cognitive Visualization Lab at IBM Watson in Cambridge, Massachusetts, USA. Martino’s data visualizations have been published in the scientific journals Nature, Science, and the Proceedings of the National Academy of Sciences. His projects have been shown at international festivals including Ars Electronica, and art galleries including the Serpentine Gallery, UK, GAFTA, USA, and the Lincoln Center, USA.<a class="showmore js-showmore">... show more</a>
+							<b>Speaker:</b> Mauro Martino is an Italian expert in data visualization based in Boston. He created and leads the Cognitive Visualization Lab at IBM Watson in Cambridge, Massachusetts, USA. Martino’s data visualizations have been published in the scientific journals Nature, Science, and the Proceedings of the National Academy of Sciences. His projects have been shown at international festivals including Ars Electronica, and art galleries including the Serpentine Gallery, UK, GAFTA, USA, and the Lincoln Center, USA.<a class="toggle-button js-toggle-button toggle-button-more"></a>
 						</p>
 					</div>
 				</div>
 		</main>
 		
 		<div class="video-wrap js-video-wrap hide">
-			<video id="talk-video" controls="" name="media"><source src="http://info.slis.indiana.edu/~stevecox/media/data-science-series/Jure_Leskovec.mp4" type="video/mp4"></video>
+			<video id="talk-video" class="video-js vjs-default-skin vjs-big-play-centered" controls preload="auto" width="640" height="360" data-setup="{}">
+      	<source src="http://info.slis.indiana.edu/~stevecox/media/data-science-series/Daniel-Katz.mp4" type="video/mp4">
+      	<p class="vjs-no-js">To view this video please enable JavaScript, and consider upgrading to a web browser that <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a></p>
+    	</video>
     	<button class="video-close js-video-close"><i class="fa fa-times" aria-hidden="true"></i></button>
     </div>
 
-		<footer>
-			<div class="container">
-				<img class="footer-logo" src="https://assets.iu.edu/brand/legacy/block-iu.png" alt="">
-				<p class="footer-text">Copyright © 2016 The Trustees of Indiana University, Copyright Complaints</p>
-				<ul class="footer-list">
-					<li class="footer-item">Privacy Notice</li>
-					<li class="footer-item">Internal Resources</li>
-					<li class="footer-item">Enter a Service Request</li>
-				</ul>
-			</div>
-		</footer>
-
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
-		<script src="js/script.js"></script>
-	</body>
-</html>
+<?php
+  include('footer.html');
+?>
